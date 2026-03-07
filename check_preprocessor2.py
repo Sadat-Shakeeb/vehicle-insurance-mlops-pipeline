0d72c0b5530723f0eb8e1cf3b37369b141bded38
Sadat-Shakeeb <sadatshakeeb@gmail.com>
2026-03-08 03:03:19 +0530

    app ready locally

--- check_preprocessor2.py
+++
@@ -1,9 +0,0 @@
-import pickle
-path = r"artifact\03_08_2026_02_14_11\data_transformation\transformed_object\preprocessing.pkl"
-with open(path, "rb") as f:
-    obj = pickle.load(f)
-print(obj)
-try:
-    print("feature names", obj.get_feature_names_out())
-except Exception as e:
-    print("cannot extract feature names:", e)
